05f41728e1f3cec2cc6a15d304a84114a6fc2b5c
ANKITA <ankitamandal19072000@gmail.com>
2021-12-10 22:37:38 +0530

    latest here

--- etssite/settings.py
+++ etssite/settings.py
@@ -133,11 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -145,3 +141,4 @@ MEDIA_URL = '/media/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
